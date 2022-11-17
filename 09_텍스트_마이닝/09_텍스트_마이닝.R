f60745e1b964697e819608387ec97993cd65bebf
suppakoko <49972015+suppakoko@users.noreply.github.com>
2022-11-18 01:14:08 +0900

    Add files via upload

---
+++ 09_텍스트_마이닝/09_텍스트_마이닝.R
@@ -0,0 +1,348 @@
+
+library(dplyr)
+library(ggplot2)
+
+#### 텍스트 마이닝(Text mining)
+## 힙합 가사 텍스트 마이닝
+
+### 텍스트 마이닝 준비하기
+#### Java 다운로드 및 설치
+#https://www.java.com/ko/download/manual.jsp
+
+
+#### 패키지 설치 및 로드
+
+# 패키지 설치
+install.packages("multilinguer")
+install.packages("rJava")
+install.packages("memoise")
+install.packages("KoNLP")
+install.packages(c("stringr","hash","tau","Sejong","RSQLite","devtools"), type = "binary")
+install.packages("remotes")
+remote::install_github("haven-jeon/KoNLP", upgrade = "never", INSTALL_opts = c("--no-multiarch"))
+multilinguer::install_jdk()
+
+
+# 패키지 로드
+library(multilinguer)
+library(KoNLP)
+library(dplyr)
+
+#### 패키지 로드 에러 발생할 경우 - java 설치 경로 확인 후 경로 설정
+
+
+########################################################################
+
+#### 사전 설정하기
+useNIADic()
+
+
+#### 데이터 준비
+# 데이터 불러오기
+txt <- readLines("hiphop.txt")
+head(txt)
+
+#### 특수문자 제거
+install.packages("stringr")
+
+library(stringr)
+
+# 특수문제 제거
+txt <- str_replace_all(txt, "\\W", " ")
+
+
+
+
+
+
+
+
+########################################################################
+### 가장 많이 사용된 단어 알아보기
+
+# 명사 추출하기
+extractNoun("대한민국의 영토는 한반도와 그 부속도서로 한다")
+
+# 가사에서 명사추출
+nouns <- extractNoun(txt)
+
+# 추출한 명사 list를 문자열 벡터로 변환, 단어별 빈도표 생성
+wordcount <- table(unlist(nouns))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+########################################################################
+#### 자주 사용된 단어 빈도표 만들기
+
+# 데이터 프레임으로 변환
+df_word <- as.data.frame(wordcount, stringsAsFactors = F)
+
+# 변수명 수정
+df_word <- rename(df_word,
+                  word = Var1,
+                  freq = Freq)
+
+# 두 글자 이상 단어 추출
+df_word <- filter(df_word, nchar(word) >= 2)
+
+top_20 <- df_word %>%
+  arrange(desc(freq)) %>%
+  head(20)
+
+
+top_20
+
+
+
+
+
+
+########################################################################
+### 워드 클라우드 만들기
+
+#### 패키지 준비하기
+
+# 패키지 설치
+install.packages("wordcloud")
+
+# 패키지 로드
+library(wordcloud)
+library(RColorBrewer)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+########################################################################
+### 워드 클라우드 만들기
+
+#### 단어 색상 목록 만들기
+pal <- brewer.pal(8,"Dark2")  # Dark2 색상 목록에서 8개 색상 추출
+
+
+#### 워드 클라우드 생성
+set.seed(1234)                   # 난수 고정
+wordcloud(words = df_word$word,  # 단어
+          freq = df_word$freq,   # 빈도
+          min.freq = 2,          # 최소 단어 빈도
+          max.words = 200,       # 표현 단어 수
+          random.order = F,      # 고빈도 단어 중앙 배치
+          rot.per = .1,          # 회전 단어 비율
+          scale = c(4, 0.3),     # 단어 크기 범위
+          colors = pal)          # 색깔 목록
+
+
+
+
+
+
+
+
+
+
+
+#################################################################
+#### 단어 색상 바꾸기
+
+pal <- brewer.pal(9,"Blues")[5:9]  # 색상 목록 생성
+set.seed(1234)                     # 난수 고정
+
+wordcloud(words = df_word$word,    # 단어
+          freq = df_word$freq,     # 빈도
+          min.freq = 2,            # 최소 단어 빈도
+          max.words = 200,         # 표현 단어 수
+          random.order = F,        # 고빈도 단어 중앙 배치
+          rot.per = .1,            # 회전 단어 비율
+          scale = c(4, 0.3),       # 단어 크기 범위
+          colors = pal)            # 색상 목록
+
+
+
+
+
+
+
+
+
+
+
+
+########################################################################
+## 국정원 트윗 텍스트 마이닝
+- 국정원 계정 트윗 데이터
+    + 국정원 대선 개입 사실이 밝혀져 논란이 됐던 2013년 6월, 독립 언론 뉴스타파가 인터넷을 통해 공개
+    + 국정원 계정으로 작성된 3,744개 트윗
+
+#### 데이터 준비하기
+
+# 데이터 로드
+twitter <- read.csv("twitter.csv",
+                    header = T,
+                    stringsAsFactors = F,
+                    fileEncoding = "UTF-8")
+
+# 변수명 수정
+twitter <- rename(twitter,
+                  no = 번호,
+                  id = 계정이름,
+                  date = 작성일,
+                  tw = 내용)
+
+# 특수문자 제거
+twitter$tw <- str_replace_all(twitter$tw, "\\W", " ")
+
+head(twitter$tw)
+
+#################################################################
+#### 단어 빈도표 만들기
+
+# 트윗에서 명사추출
+nouns <- extractNoun(twitter$tw)
+
+# 추출한 명사 list를 문자열 벡터로 변환, 단어별 빈도표 생성
+wordcount <- table(unlist(nouns))
+
+# 데이터 프레임으로 변환
+df_word <- as.data.frame(wordcount, stringsAsFactors = F)
+
+# 변수명 수정
+df_word <- rename(df_word,
+                  word = Var1,
+                  freq = Freq)
+
+
+
+
+
+
+
+
+
+
+
+#################################################################
+#### 두 글자 이상으로 된 단어 추출, 빈도 상위 20개  단어 추출
+
+# 두 글자 이상 단어만 추출
+df_word <- filter(df_word, nchar(word) >= 2)
+
+# 상위 20개 추출
+top20 <- df_word %>%
+  arrange(desc(freq)) %>%
+  head(20)
+
+top20
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#################################################################
+#### 단어 빈도 막대 그래프 만들기
+
+library(ggplot2)
+
+order <- arrange(top20, freq)$word               # 빈도 순서 변수 생성
+
+ggplot(data = top20, aes(x = word, y = freq)) +
+  ylim(0, 2500) +
+  geom_col() +
+  coord_flip() +
+  scale_x_discrete(limit = order) +              # 빈도 순서 변수 기준 막대 정렬
+  geom_text(aes(label = freq), hjust = -0.3)     # 빈도 표시
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#################################################################
+#### 워드 클라우드 만들기
+
+pal <- brewer.pal(8,"Dark2")       # 색상 목록 생성
+set.seed(1234)                     # 난수 고정
+
+wordcloud(words = df_word$word,    # 단어
+          freq = df_word$freq,     # 빈도
+          min.freq = 10,           # 최소 단어 빈도
+          max.words = 200,         # 표현 단어 수
+          random.order = F,        # 고빈도 단어 중앙 배치
+          rot.per = .1,            # 회전 단어 비율
+          scale = c(6, 0.2),       # 단어 크기 범위
+          colors = pal)            # 색상 목록
+
+
+
+
+
+
+
+
+
+
+
+
+
+#################################################################
+#### 색깔 바꾸기
+
+pal <- brewer.pal(9,"Blues")[5:9]  # 색상 목록 생성
+set.seed(1234)                     # 난수 고정
+
+wordcloud(words = df_word$word,    # 단어
+          freq = df_word$freq,     # 빈도
+          min.freq = 10,           # 최소 단어 빈도
+          max.words = 200,         # 표현 단어 수
+          random.order = F,        # 고빈도 단어 중앙 배치
+          rot.per = .1,            # 회전 단어 비율
+          scale = c(6, 0.2),       # 단어 크기 범위
+          colors = pal)            # 색상 목록
+
+
+
+
+
+
+
+
+
+
---
+++ 15_제어문/15_R_제어문.R
@@ -0,0 +1,437 @@
+#############################################################
+## 제어문 
+###########
+if(!require(dplyr)) install.packages("dplyr"); library(dplyr)
+
+###################################################################
+# x가 0보다 크면 양수
+x <- 7
+if (x > 0) {
+    print("양수")
+}
+
+###################################################################
+# x가 0보다 크면 양수, 그렇지 않으면 음수
+x <- -3
+if (x > 0) {
+    print("양수")
+} else {
+    print("음수")
+}
+
+
+###################################################################
+# x가 90 이상이면 A, ..., 60 이상이면 D, 나머지(60미만)는 F
+x <- 92
+if (x >= 90) {
+    print("A")
+} else if (x >= 80) {
+    print("B")
+} else if (x >= 70) {
+    print("C")
+} else if (x >= 60) {
+    print("D")
+} else {
+    print("F")
+}
+
+###################################################################
+# {}를 사용하지 않는 코드는 나중에 이해하기 어려우므로 추천하지 않습니다.
+x <- 5
+if (x > 0) print("양수") else print("음수")
+
+
+
+
+
+
+
+
+
+###################################################################
+# 조건이 2개 이상이 있는 경우 논리연산자를 사용
+x <- 25
+y <- 75
+if (x > 0 & y < 100) {
+    print("TRUE 입니다")
+} else {
+    print("FALSE 입니다")
+}
+
+
+
+
+
+
+
+
+######################################################
+# ifelse()
+
+x <- 5
+ifelse(x %% 2 == 0, "짝수", "홀수")
+
+
+x <- c(1, 2, 3, 4, 5, NA, 7)
+ifelse(x %% 2 == 0, "짝수", "홀수")
+
+
+
+
+
+
+
+
+###################################################################
+#switch
+
+x <- "한국인"
+
+switch(x,
+       "영국인" = print("영국인입니다!"),
+       "한국인" = print("한국인입니다!"),
+       "미국인" = print("미국인입니다!"),
+       stop("지시값이 정확하지 않습니다!")
+)
+
+
+x <- 2
+switch(x,
+       "영국인" = print("영국인입니다!"),
+       "한국인" = print("한국인입니다!"),
+       "미국인" = print("미국인입니다!"),
+       stop("지시값이 정확하지 않습니다!")
+)
+
+
+
+
+
+
+
+
+
+##########################################################
+## 연습문제
+
+Q1. 변수 x에 1,2,3,4 중의 한 수가 저장되어있다. 
+1일 때는 "one", 2일 때는 "two", 그 외에는 "else!"라고 출력하는 R 스크립트를 작성하세요
+
+
+
+
+
+Q2. 변수 ch에 문자가 저장되어있다. 
+'a'또는 'b'일 때는 1을, 'c'또는 'd' 일 때는 3을, 그 밖의 경우에는 0을 출력하세요.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+###################################################################
+## for
+
+# 1부터 5까지 출력
+for (i in 1:5) {
+    print(i)
+}
+
+
+
+# 알파벳 철자 순서대로 5개 출력, x의 각 요소를 출력
+x <- LETTERS[1:5]
+for (i in 1:5) {
+    print(x[i])
+}
+
+
+# for 함수에서 사용된 아이템 변수 i는 변함
+i <- 55
+print(i)
+
+
+for (i in 1:3) {
+    print(paste0(i, "입니다~"))
+}
+
+print(i)
+
+
+# break
+for (variable in 1:5) {
+  if (variable == 4) {
+    print("break")
+    break
+  }
+}
+variable
+
+# 3미만이면 이후 명령을 실행하지 않고 다음으로 넘어가고
+# 5초과이면 for 반복에서 빠져 나옴
+for (i in 1:10) {
+    if (i < 3) {
+        next
+    }
+    print(i)
+    if (i >= 5) {
+        break
+    }
+}
+
+
+
+means <- c(1, 30, 50)
+sds <- c(1, 2, 3)
+out <- vector("list", length(means))
+for (i in seq_along(means)) {
+    out[[i]] <- rnorm(5, means[[i]], sds[[i]])
+}
+print(out)
+
+
+
+### matrix에서 이중 for 문으로 요소 출력하기
+x <- matrix(1:6, nrow = 2, ncol = 3)
+x
+
+for (i in seq_len(nrow(x))) {
+    for (j in seq_len(ncol(x))) {
+        print(x[i, j])
+    }
+}
+
+
+# assign get example
+df <- data.frame(var1 = c("사과","딸기","바나나"), 
+                 var2 = c("주스","아이스크림","우유"))
+
+for (i in 1:length(df$var1)){
+  assign(df$var1[i], df$var2[i]) # assign을 이용해서 var2의 데이터를 var1의 항목의 이름으로 변수 선언
+}
+
+for (i in 1:length(df$var1)){
+  var <- get(df$var1[i]) #get을 이용해서 var1의 함목의 이름을 가진 변수의 데이터를 출력
+  print(var)
+}
+
+###################################################################
+## while
+
+i <- 1
+while (i < 3) {
+    print(i)
+    i <- i + 1
+}
+
+
+count <- 3
+while (count <= 5) {
+    print(c(count, count - 1, count - 2))
+    count <- count + 1
+}
+
+
+set.seed(123)
+x <- 5
+while (x >= 3 & x <= 10) {
+    coin <- rbinom(1, 1, 0.5)  # 0과 1 무작위로 추출
+    if (coin == 1) {
+        x <- x + 1
+    } else {
+        x <- x - 1
+    }
+}
+print(x)
+
+
+
+
+
+
+
+
+
+
+###################################################################
+## repeat()
+
+# 1 ~ 5까지 출력 (x가 6이되면 반복 종료)
+x <- 1
+repeat {
+    print(x)
+    x <- x + 1
+    if (x == 6) { break }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##########################
+## 제어문 예제
+
+하나에 엑셀파일에 있는 여러개의 시트 한번에 읽어오기
+
+library(readxl)
+
+rm(list=ls()) #모든 변수 제거하기
+setwd('RMD/data_analysis_class/')
+
+fn <- "15_제어문/multi_sheet_read_exemple.xlsx"
+vSh <- excel_sheets(fn)
+vSh # excel sheet 이름 추출
+
+
+if (length(vSh) > 0) { # sheet 가 0이 아닐때
+  for (iSh in 1:(length(vSh))){ # sheet 개수 만큼 반복
+    vname <- vSh[iSh] # sheet 이름 vname에 저장
+    if (exists(vname)){ # 기존에 sheet이름과 일치하는 변수 존재 확인
+      cat("\b\b변수","vname","이(가) 이미 존재합니다.\n") # 기존 변수가 sheet이름과 동일하면 부르지 않기
+      break
+    }
+    assign(vname, read_excel(fn, sheet = vSh[iSh])) # assign() 함수로 변수이름과 변수 데이터 읽어서 연결하기
+  }
+} else {
+  cat("No sheet!!!\n")
+}
+
+
+
+###################################################################
+### for 문을 이용하여 위 엑셀파일에서 불러온 데이터 프레임 covid_19_data, ds_salaries, wdbc을 각각의 tab으로 분리된 각 sheet의 이름으로 된 csv 파일로 저장하시오
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+###########################################
+## 함수 정의하기
+
+s <- 0
+for (i in 1:5){
+    s <- s + i
+}
+print(s)
+
+s <- 0
+for (i in 1:10){
+    s <- s + i
+}
+print(s)
+
+
+s <- 0
+for (i in 1:20){
+    s <- s + i
+}
+print(s)
+
+# 공통부분을 확인하여, 달라지는 부분을 변수로 치환한다.
+
+sumToN <- function(n){
+    s<-0
+    for (i in 1:n){
+        s <- s + i}
+    return(s)
+    }
+
+sumToN(5)
+
+
+
+
+
+
+###################################################################
+### 평균, 중위수 등을 선택해서 사용하도록 하는 함수
+
+center <- function(x, type) {
+    switch(type,
+           mean = mean(x),
+           median = median(x),
+           trimmed = mean(x, trim = .1),
+           stop("type 값을 잘못 입력하였습니다!")
+    )
+}
+set.seed(123)
+
+x <- sample(x=1:30, size=10, replace=T)
+
+center(x, "mean")
+center(x, "median")
+center(x, 2)  # median 호출
+
+
+
+
+
+
+###################################################################
+### year를 기준으로 평균을 내주는 사용자 함수 작성
+group_mean <- function(df, col_names) {
+    df %>% 
+        group_by(year) %>% 
+        summarise(mean(get(col_names)))
+}
+
+group_mean(storms, "wind")
+
+group_mean(storms, "pressure")
+
+group_mean(storms, wind) # wind를 변수로 인식
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
